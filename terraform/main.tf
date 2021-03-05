211a32b9ae5e0b953489575ed0f3f1ac0fd295a9
Greg Walker <michael.walker@gsa.gov>
2021-03-05 10:39:12 -0600

    move terraform state into an S3 bucket, so it doesn't just live on my machine

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,11 @@
 terraform {
+  backend "s3" {
+    bucket  = "1337-nasa-sbir-terraform"
+    key     = "terraform.tfstate"
+    profile = "tts-sandbox"
+    region  = "us-east-2"
+  }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
