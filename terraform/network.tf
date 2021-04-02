10425eebd773fc77da814518369752ed9926fa6e
Greg Walker <michael.walker@gsa.gov>
2021-04-02 12:06:57 -0500

    switch back to 10.x.x.x CIRD

--- terraform/network.tf
+++ terraform/network.tf
@@ -3,7 +3,7 @@ data "aws_availability_zones" "available" {
 }
 
 resource "aws_vpc" "main" {
-  cidr_block           = "172.31.0.0/16" #"10.0.0.0/16"
+  cidr_block           = "10.0.0.0/16"
   enable_dns_support   = true
   enable_dns_hostnames = true
   tags = merge(
