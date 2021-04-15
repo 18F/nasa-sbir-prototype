67ea4ad4886724f7ec020fcf9b7fad0f13428fb3
Greg Walker <michael.walker@gsa.gov>
2021-04-15 15:53:29 -0500

    a bit more cleaning

--- initial-build.sh
+++ initial-build.sh
@@ -13,13 +13,15 @@ fi
 cd terraform
 terraform apply --var-file=../variables.tfvars.json -auto-approve
 
+PROFILE="$(terraform output -raw aws_profile)"
+REGION="$(terraform output -raw aws_region)"
 REGISTRY_URL="$(terraform output -raw container_registry_url)"
 
 # Log Docker into the AWS ECR registry
 aws ecr \
-  --profile="$(terraform output -raw aws_profile)" \
+  --profile="$PROFILE" \
   get-login-password \
-  --region "$(terraform output -raw aws_region)" | \
+  --region "$REGION" | \
   docker login \
   --username AWS \
   --password-stdin \
@@ -32,25 +34,20 @@ docker tag "$REGISTRY_URL":1.0 "$REGISTRY_URL":latest
 docker push "$REGISTRY_URL":latest
 docker push "$REGISTRY_URL":1.0
 
-# Migrate the database schema
-aws ecs \
-  --profile="$(terraform output -raw aws_profile)" \
-  run-task \
-  --task-definition "$(terraform output -raw migration_task_definition)" \
-  --cluster "$(terraform output -raw api_cluster)" \
-  --network-configuration "awsvpcConfiguration={subnets=[$(terraform output -raw subnet_private)],securityGroups=[$(terraform output -raw task_security_group)],assignPublicIp=DISABLED}" \
-  --launch-type FARGATE
+run_task () {
+  aws ecs \
+    --profile="$PROFILE" \
+    run-task \
+    --task-definition "$(terraform output -raw $1_task_definition)" \
+    --cluster "$(terraform output -raw api_cluster)" \
+    --network-configuration "awsvpcConfiguration={subnets=[$(terraform output -raw subnet_private)],securityGroups=[$(terraform output -raw task_security_group)],assignPublicIp=DISABLED}" \
+    --launch-type FARGATE
+}
 
+# Migrate the database schema and then seed it with data
+run_task "migration"
 #### --- need to wait here for the migration to finish ---
-
-# Seed the database with initial set of stuff
-aws ecs \
-  --profile="$(terraform output -raw aws_profile)" \
-  run-task \
-  --task-definition "$(terraform output -raw seed_task_definition)" \
-  --cluster "$(terraform output -raw api_cluster)" \
-  --network-configuration "awsvpcConfiguration={subnets=[$(terraform output -raw subnet_private)],securityGroups=[$(terraform output -raw task_security_group)],assignPublicIp=DISABLED}" \
-  --launch-type FARGATE
+run_task "seed"
 
 API_URL="http://$(terraform output -raw api_dns)"
 
