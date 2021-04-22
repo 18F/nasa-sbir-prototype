86a62bc665cda7afbf18a129d831589084e208d1
Greg Walker <michael.walker@gsa.gov>
2021-04-22 10:22:01 -0500

    combined post-phase-2s into a single "2*"

--- api/web/subtopic.js
+++ api/web/subtopic.js
@@ -10,12 +10,65 @@ const subtopics = new Promise((resolve, reject) => {
     .catch(() => reject());
 });
 
+const squashPostPhaseTwo = (subtopic) => {
+  const postPhaseTwos = [];
+  for (let i = 0; i < subtopic.phases.length; i += 1) {
+    if (subtopic.phases[i].phase.match(/^2.$/)) {
+      postPhaseTwos.push(...subtopic.phases.splice(i, 1));
+      i -= 1;
+    }
+  }
+  const postPhaseTwo = postPhaseTwos.reduce(
+    (o, { awards, proposals, years }) => ({
+      ...o,
+      awards: o.awards + awards,
+      proposals: o.proposals + proposals,
+      years: [...o.years, ...years],
+    }),
+    {
+      awards: 0,
+      phase: "2*",
+      proposals: 0,
+      ratio: 0,
+      years: [],
+    }
+  );
+
+  const postPhaseTwoYears = [];
+  new Set(postPhaseTwo.years.map(({ year }) => year)).forEach((year) => {
+    const data = postPhaseTwo.years.filter(
+      ({ year: proposalYear }) => proposalYear === year
+    );
+
+    const combinedYear = data.reduce(
+      (o, { awards, proposals }) => ({
+        awards: o.awards + awards,
+        proposals: o.proposals + proposals,
+        ratio: 0,
+        year,
+      }),
+      { awards: 0, proposals: 0 }
+    );
+    postPhaseTwoYears.push(combinedYear);
+  });
+  postPhaseTwo.years = postPhaseTwoYears;
+
+  for (let i = 0; i < subtopic.phases.length; i += 1) {
+    if (subtopic.phases[i].phase.match(/^3$/)) {
+      subtopic.phases.splice(i, 0, postPhaseTwo);
+      i += 1;
+    }
+  }
+};
+
 const setSubtopic = async (app, id) => {
   const response = await fetch(`${API_URL}/v1/subtopics/${id}?byYear&byPhase`);
-  const subtopic = await response.json();
+  const subtopic = (await response.json()).pop();
+
+  squashPostPhaseTwo(subtopic);
 
   app.view = "single_subtopic";
-  app.subtopic = subtopic.pop();
+  app.subtopic = subtopic;
   window.scrollTo(0, 0);
 
   const graphOptions = {
